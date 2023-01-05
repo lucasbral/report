7e8fab8b73783c59956e89fb9ceb7df7685f2a88
lucasbral <lucasdecarvalhosobral@gmail.com>
2023-01-05 18:57:53 -0300

    v3.0

---
+++ dashboard.js
@@ -0,0 +1,12 @@
+/* globals Chart:false, feather:false */
+
+(() => {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  
+})()
